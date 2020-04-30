e30429fab2d086219800e921663fa33db348e74b
mingyan <wenmy@tuya.com~>
2020-04-30 15:47:20 +0800

    update example

---
+++ Example/Pods/Target Support Files/Pods-MYIconfont_Example/Pods-MYIconfont_Example-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MYIconfont_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MYIconfont_ExampleVersionString[];
+
